5dc6482ee057bd4fb7db556d4fc8961b54f00e88
Jeremy Muhlich <jmuhlich@bitflood.org>
2010-11-04 12:01:58 -0400

    added prompting on stderr when no input files specified

--- filter.pl
+++ filter.pl
@@ -1,5 +1,9 @@
 #!/usr/bin/perl
 
+if (!@ARGV) {
+  print STDERR "reading mbox contents from stdin...\n";
+}
+
 $_ = join('', <>);
 
 s/(?<!^)(?<!\n\n)(From [^@]+@[^ ]+  \w{3} \w{3} (?: |\d)\d \d\d:\d\d:\d\d \d{4})(?=\n)/\n\n$1/g;
