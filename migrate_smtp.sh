2c2167226bfe0a6659c6b1fb1044601adc6866cd
Jeremy Muhlich <jmuhlich@bitflood.org>
2010-11-04 12:02:24 -0400

    added driver shell script

---
+++ migrate_smtp.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+MBOX=$1
+LIST=$2
+
+if [ -z "$MBOX" -o -z "$LIST" ]; then
+  cat <<-USAGE
+	Usage: $(basename $0) oldlist.mbox newlist@example.com
+	Clean up a MailMan archive and resend all messages to a new list.
+	USAGE
+  exit 1
+fi
+
+perl filter.pl $MBOX | formail -s sendmail $LIST
